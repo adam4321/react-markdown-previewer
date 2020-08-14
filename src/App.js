ea46c4d1ca4dbd9e774205ffefbdac6ed1b4d2bd
Adam Wright <adam@adamjwright.com>
2020-08-14 01:56:59 -0700

    force updates

--- src/App.js
+++ src/App.js
@@ -24,22 +24,22 @@ class App extends React.Component {
     render() {
         let { value } = this.state;
         return (
-            <div className = "row">
-                <div className = "one-half column markdown-div">
+            <div className="row">
+                <div className="one-half column markdown-div">
                     <textarea
-                        className = "markdown-input"
-                        onChange = {this.handleChange}
-                        id = "editor"
-                        ref = "editor"
-                        defaultValue = {value}
+                        className="markdown-input"
+                        onChange={this.handleChange}
+                        id="editor"
+                        ref="editor"
+                        defaultValue={value}
                     >
                     </textarea>
-                    <button id = "back-button" onClick = {() => window.history.back()}> Back </button>
+                    <button id="back-button" onClick={() => window.history.back()}> Back </button>
                 </div>
                 <div
-                    id = "preview"
-                    className = "one-half column"
-                    dangerouslySetInnerHTML = {this.createMarkup()}
+                    id="preview"
+                    className="one-half column"
+                    dangerouslySetInnerHTML={this.createMarkup()}
                 >
                 </div>
             </div>
